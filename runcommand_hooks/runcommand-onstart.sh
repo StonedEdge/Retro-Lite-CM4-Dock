286733311660591145de545d7b219a27dab21427
StonedEdge <45709395+StonedEdge@users.noreply.github.com>
2023-02-10 17:13:20 +0900

    Update runcommand-onstart.sh

--- runcommand_hooks/runcommand-onstart.sh
+++ runcommand_hooks/runcommand-onstart.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/env bash
 echo $(date -u +%Y-%m-%dT%H:%M:%S%z)'|'start'|'$1'|'$2'|'$3'|'$4 >> ~/RetroPie/game_stats.log
+
+# Clear out retropie-oled.log previous ROM directory data
+> /tmp/retropie-oled.log
+
+# Get the system name
+system=$1
+
+# Get the full path filename of the ROM and store it in rom
+rom=$3
+
+# rom_bn receives $rom excluding everything from the first char to the last slash '/'
+rom_bn="${rom##*/}"
+
+GAMELIST1="/home/pi/RetroPie/roms/${system}/gamelist.xml"
+GAMELIST2="/home/pi/.emulationstation/gamelists/${system}/gamelist.xml"
+
+if [ -f ${GAMELIST1} ]
+then
+GAMELIST=${GAMELIST1}
+else
+GAMELIST=${GAMELIST2}
+fi
+
+title=`grep -s -w -A1 "${rom_bn}" ${GAMELIST} | awk '{getline;print}' | awk 'BEGIN {FS="<name>"} {print $2}' | awk 'BEGIN {FS="</name>"} {print $1}'`
+title="${title%%(*}"
+rom_bn="${rom_bn%.*}"
+
+echo "$HOME/RetroPie/roms/$1/media/screenshot/${rom_bn}.png" >> /tmp/retropie-oled.log
+echo "$HOME/RetroPie/roms/$1/media/wheel/${rom_bn}.png" >> /tmp/retropie-oled.log
+echo $1 >> /tmp/retropie-oled.log
