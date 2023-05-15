de09b21d8bb8c7e33b009c2c46ecd1b2649adcf5
StonedEdge <45709395+StonedEdge@users.noreply.github.com>
2023-05-16 01:36:54 +0900

    Implement sleep mode after min of inactivity

--- src/main.c
+++ src/main.c
@@ -15,8 +15,9 @@
 #include "splash_vid.h"
 
 #define IMAGE_SIZE_BYTES 32768
+#define INACTIVE_TIMEOUT 60 * 1000000
 #define button_pin 6
-#define debounce_time 100
+#define debounce_time_ms 100
 
 // The worst case for an uncomporessed IPV6 address would be 8 4-digit segments plus 7 colon separators + null, or 40 chars.
 // I've reserved 64 characters just in case, although that would never fit in the tiny display.
@@ -29,46 +30,54 @@ uint8_t boxart_buffer[IMAGE_SIZE_BYTES] = { 0x00 };
 uint8_t combined_buffer[IMAGE_SIZE_BYTES] = { 0x00 };
 uint8_t consol_buffer[IMAGE_SIZE_BYTES] = { 0x00 };
 
+void EnableDisplay(bool enable);
 bool GetLine(char* line, int lineSize);
 
 typedef enum { SPLASH_SCREEN, GAME_ACTIVE, DISPLAY_STATS } DISPLAY_MODE;
-volatile DISPLAY_MODE display_mode = SPLASH_SCREEN;
+volatile DISPLAY_MODE displayMode = SPLASH_SCREEN;
+volatile bool displayEnabled = false;   // The display is actually on after initialization, but set to false for EnableDisplay.
+uint32_t lastActivityTime;              // Last activity in us
 
-/**
+                                        /**
 * Display the various images in rotation while in "GAME_ACTIVE" mode.
 */
 
-void button_loop(void) {
-    extern volatile DISPLAY_MODE display_mode;
+void ButtonLoop(void) {
+    extern volatile DISPLAY_MODE displayMode;
+    extern uint32_t lastActivityTime;
+
+    EnableDisplay(true);
 
     int current_display = 0; // start with the first image
     uint32_t last_press_time = 0;
 
-    while (display_mode == GAME_ACTIVE) {
+    while (displayMode == GAME_ACTIVE) {
         if (!gpio_get(button_pin)) {
-            uint32_t current_time = time_us_32();
-            if (current_time - last_press_time >= debounce_time * 1000) {
-                last_press_time = current_time;
+            uint32_t currentTime = time_us_32();
+            lastActivityTime = currentTime;
+
+            if (currentTime - last_press_time >= debounce_time_ms * 1000) {
+                last_press_time = currentTime;
 
                 // Button is pressed, cycle through the displays
-                current_display++;
-                if (current_display > 2) {
+
+                if (++current_display > 2) {
                     current_display = 0; // cycle back to the first display
                 }
+
+                EnableDisplay(true);
+
                 switch (current_display) {
-                    case 0:
+                    case 0:  // Display the combined image"
                         SSD1351_clear_8();
-                        // Display the combined image"
                         SSD1351_display_image(combined_buffer);
                         break;
-                    case 1:
+                    case 1:  // Display the boxart image
                         SSD1351_clear_8();
-                        // Display the boxart image
                         SSD1351_display_image(boxart_buffer);
                         break;
-                    case 2:
+                    case 2:  // Display the consol image
                         SSD1351_clear_8();
-                        // Display the consol image
                         SSD1351_display_image(consol_buffer);
                         break;
                 }
@@ -76,6 +85,10 @@ void button_loop(void) {
         }
 
         sleep_ms(10); // wait a short time before checking again
+
+        if (time_us_32() - lastActivityTime >= INACTIVE_TIMEOUT) {
+            EnableDisplay(false);
+        }
     }
 }
 
@@ -88,11 +101,13 @@ static char IP_addr[STATS_BUFFER_SIZE];     // E
 // The previous values are used so that we'll have something to init the display with on the first call when entering "X" mode.
 // TODO: This claim is a bit funky - do we really need these?
 
+#if 1
 char prev_SD_usage[STATS_BUFFER_SIZE];      // A
 char prev_CPU_temp[STATS_BUFFER_SIZE];      // B
 char prev_CLK_speed[STATS_BUFFER_SIZE];     // C
 char prev_RAM_usage[STATS_BUFFER_SIZE];     // D
 char prev_IP_addr[STATS_BUFFER_SIZE];       // E
+#endif
 
 /**
 * Display statistics.
@@ -104,11 +119,14 @@ char prev_IP_addr[STATS_BUFFER_SIZE];       // E
 */
 
 void DisplayStats(void) {
+#if 1
     // TODO: Do we need the "prev" values??
+#endif
 
     extern char SD_usage[STATS_BUFFER_SIZE], CPU_temp[STATS_BUFFER_SIZE], CLK_speed[STATS_BUFFER_SIZE], RAM_usage[STATS_BUFFER_SIZE], IP_addr[STATS_BUFFER_SIZE];
+#if 1
     extern char prev_SD_usage[STATS_BUFFER_SIZE], prev_CPU_tempSTATS_BUFFER_SIZE, prev_CLK_speed[STATS_BUFFER_SIZE], prev_RAM_usage[STATS_BUFFER_SIZE], prev_IP_addr[STATS_BUFFER_SIZE];
-
+#endif
     const int r = 255;                  // Title colour
     const int g = 255;
     const int b = 255;
@@ -117,7 +135,11 @@ void DisplayStats(void) {
     const int g1 = 180;
     const int b1 = 180;
 
-    // Clear Values  TODO: "Clear"??
+#if 1
+    // Clear Values
+    // NOTE WELL:  The SSD1351_write_char() function only sets non-blank pixel, hence this code.
+    // How about changing that routine to write all pixels?
+
     SSD1351_set_cursor(5, 17);
     SSD1351_printf(SSD1351_get_rgb(0, 0, 0), small_font, prev_SD_usage);
     SSD1351_set_cursor(5, 42);
@@ -134,6 +156,7 @@ void DisplayStats(void) {
     strncpy(prev_CLK_speed, CLK_speed, strlen(CLK_speed));
     strncpy(prev_RAM_usage, RAM_usage, strlen(RAM_usage));
     strncpy(prev_IP_addr, IP_addr, strlen(IP_addr));
+#endif
 
     // Display Values
     SSD1351_set_cursor(5, 5);
@@ -157,6 +180,56 @@ void DisplayStats(void) {
     SSD1351_set_cursor(5, 117);
     SSD1351_printf(SSD1351_get_rgb(r1, g1, b1), small_font, IP_addr);
 
+    SSD1351_update();
+}
+
+/**
+* Enable or disable the display.
+* 
+* On enable we also reset lastActivityTime, which is used for turning the display off after inactivity.
+* 
+* @param enable         [in] true to enable the display.
+*/
+
+void EnableDisplay(bool enable)
+{
+    extern volatile bool displayEnabled;
+    extern uint32_t lastActivityTime;
+
+    if (displayEnabled != enable) {
+        displayEnabled = enable;
+
+        if (enable) {
+            SSD1351_write_command(SSD1351_CMD_FUNCTIONSELECT);  // Enable Vdd voltage regulator
+            SSD1351_write_data(0x01);
+            SSD1351_write_command(SSD1351_CMD_DISPLAYON);
+        }
+        else {
+            // NOTE WELL:  You cannot write the display memory while the voltage regulator is off.
+
+            SSD1351_write_command(SSD1351_CMD_DISPLAYOFF);
+            SSD1351_write_command(SSD1351_CMD_FUNCTIONSELECT);  // Disable Vdd voltage regulator to save power
+            SSD1351_write_data(0x00);
+        }
+    }
+
+    if (enable) {  // Reset the last activity time elsewhere we can determine inactivity.
+        lastActivityTime = time_us_32();
+    }
+}
+
+/**
+* Switch from current display mode to DISPLAY_STATS mode.
+* 
+* Called when we get the first "X" command and after an END command.
+*/
+
+void EnableDisplayStats()
+{
+    displayMode = DISPLAY_STATS;
+
+    multicore_reset_core1();
+    SSD1351_clear_8();
     SSD1351_update();
 }
 
@@ -191,7 +264,14 @@ bool GetLine(char* line, int lineSize)
     }
 }
 
-void play_splash_vid(void) {
+/**
+* Play the splash screen for up to 20 seconds.
+*/
+
+void PlaySplashVid(void) 
+{
+    EnableDisplay(true);
+
     int t = 0;
     while (t < 20) {
         SSD1351_write_command(SSD1351_CMD_WRITERAM);
@@ -214,7 +294,13 @@ void play_splash_vid(void) {
     SSD1351_update();
 }
 
-void splash_vid_single(void) {
+/**
+* Play the splash screen video once.  (Not used.)
+*/
+
+void PlaySplashVidSingle(void) {
+    EnableDisplay(true);
+
     SSD1351_write_command(SSD1351_CMD_WRITERAM);
     SSD1351_write_data_buffer(frame[0], OLED_BUF_SIZE);
     sleep_ms(500);
@@ -239,7 +325,9 @@ void splash_vid_single(void) {
 */
 
 void SerialThread(void) {
-    extern volatile DISPLAY_MODE display_mode;
+    extern volatile DISPLAY_MODE displayMode;
+    extern volatile bool displayEnabled;
+    extern uint32_t lastActivityTime;
     extern char SD_usage[STATS_BUFFER_SIZE], CPU_temp[STATS_BUFFER_SIZE], CLK_speed[STATS_BUFFER_SIZE], RAM_usage[STATS_BUFFER_SIZE], IP_addr[STATS_BUFFER_SIZE];
 
     // Check if there's any serial input available
@@ -248,11 +336,10 @@ void SerialThread(void) {
 
         while (GetLine(line, sizeof line)) {
             if (strcmp(line, "X") == 0) {
-                if (display_mode != DISPLAY_STATS) {
-                    multicore_reset_core1();
-                    SSD1351_clear_8();
-                    SSD1351_update();
-                    display_mode = DISPLAY_STATS;
+                if (displayMode == SPLASH_SCREEN) {  // We've just started, show statistics now.
+                    EnableDisplayStats();
+                    EnableDisplay(true);
+                    DisplayStats();
                 }
             }
             else if (strcmp(line, "start") == 0) {
@@ -262,17 +349,15 @@ void SerialThread(void) {
                 SSD1351_get_image(boxart_buffer);
                 SSD1351_get_image(consol_buffer);
                 SSD1351_display_image(combined_buffer);
-                display_mode = GAME_ACTIVE;
-                multicore_launch_core1(button_loop);
+                displayMode = GAME_ACTIVE;
+                multicore_launch_core1(ButtonLoop);  // ButtonLoop manages lastActivityTime
             }
             else if (strcmp(line, "END") == 0) {
-                multicore_reset_core1();
-                SSD1351_clear_8();
-                SSD1351_update();
-                display_mode = DISPLAY_STATS;
-                DisplayStats();         // Redraw (possibly empty data).
+                EnableDisplayStats();
+                EnableDisplay(true);
+                DisplayStats();
             }
-            else if (display_mode == DISPLAY_STATS) {
+            else {
                 if (line[0] == 'A') {
                     line[STATS_BUFFER_SIZE - 1] = '\0'; // Truncate if too long
                     strcpy(SD_usage, line);
@@ -292,7 +377,22 @@ void SerialThread(void) {
                 else if (line[0] == 'E') {
                     line[STATS_BUFFER_SIZE - 1] = '\0'; // Truncate if too long
                     strcpy(IP_addr, line);
-                    DisplayStats();         // We should have all five items now -- update the display.
+
+                    // This should allow the stats to be displayed after an END command, and then turn the screen off.
+                    // The only way to turn the screen back on is then to start another game.
+
+                    if (displayMode == DISPLAY_STATS) {
+                        if (displayEnabled) {
+                            uint32_t currentTime = time_us_32();
+                            if (currentTime - lastActivityTime >= INACTIVE_TIMEOUT) {
+                                EnableDisplay(false);
+                            }
+                        }
+
+                        if (displayEnabled) {
+                            DisplayStats();
+                        }
+                    }  // Not in stats mode, just save the data.
                 }
             }  // Else nonsense input
         }  // GetLine() failed - probably host went away.
@@ -301,11 +401,13 @@ void SerialThread(void) {
     }
 }
 
+
 int main(void)
 {
-    extern volatile DISPLAY_MODE display_mode;
+    extern volatile DISPLAY_MODE displayMode;
 
     // GPIO Set-Up
+
     spi_set_format(SPI_PORT, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
     gpio_init(CS);
     gpio_set_dir(CS, GPIO_OUT);
@@ -323,13 +425,15 @@ int main(void)
     gpio_pull_up(button_pin);
 
     // SPI Initialisation
+
     stdio_init_all();
     spi_init(SPI_PORT, 15000000);
     SSD1351_init();
 
     // Set up the video thread
-    multicore_launch_core1(play_splash_vid);
-    display_mode = SPLASH_SCREEN;
+
+    displayMode = SPLASH_SCREEN;
+    multicore_launch_core1(PlaySplashVid);  // PlaySplashVid calls EnableDisplay.
 
     while (1) {
         SerialThread();
