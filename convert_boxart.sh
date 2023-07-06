3f071253e45413b45a995e67bdd345f10028169e
StonedEdge <45709395+StonedEdge@users.noreply.github.com>
2023-07-06 17:03:54 +0900

    Update convert_boxart.sh

--- convert_boxart.sh
+++ convert_boxart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Traverse all subdirectories in the given directory and convert JPEG images
-# to PNG format
+# Benjamin David Todd
+# Traverse all subdirectories in the given games directory and convert JPEG images to PNG format
 find /opt/retropie/configs/all/emulationstation/downloaded_images -type d | while read dir; do
   find "$dir" -maxdepth 1 -type f -name "*.jpg" -print0 | while read -d $'\0' file; do
     echo "Converting $file to PNG..."
